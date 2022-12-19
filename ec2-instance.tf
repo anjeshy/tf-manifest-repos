7dd077fe1e90ffe4d84631fd7cb920aba8f46b46
unknown <anjesh.yashodharan@gmail.com>
2022-12-19 13:06:49 +0530

    Added

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "4.46.0"
+      version = "4.47.0"
     }
   }
 }
@@ -18,8 +18,7 @@ provider "aws" {
 resource "aws_instance" "web_server" {
   ami = "ami-0b0dcb5067f052a63"
   instance_type = "t2.micro"
-  user_data = file("${path.module}/app1-install.sh")
   tags = {
-    "Name" = "Nova-WebServer"
+    "Name" = "Anjesh-WebServer"
   }
 }
